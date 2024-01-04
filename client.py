5d97d26b74b9f497cdbe46ce9a28eff18452cfa5
Denis <denis.frincu02@e-uvt.ro>
2024-01-04 16:03:30 +0200

    delete file

--- client.py
+++
@@ -1,8 +0,0 @@
-class Client:
-    def __init__(self, client_id, thread, status='active'):
-        self.client_id = client_id
-        self.thread = thread
-        self.status = status
-
-    def __str__(self):
-        return f"Client {self.client_id} - Status: {self.status}"
