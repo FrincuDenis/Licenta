f6c2d97ba3255dab3923a16b3028525db7c20d84
Denis Frincu <denis.frincu02@e-uvt.ro>
2024-05-22 22:50:08 +0300

    status.py better version on power metrics

--- client/hardware/status.py
+++ client/hardware/status.py
@@ -1,40 +1,27 @@
 import psutil
-import clr  # the pythonnet module
 import threading
 import time
 
-clr.AddReference(r'OpenHardwareMonitorLib')
-# e.g. clr.AddReference(r'OpenHardwareMonitor/OpenHardwareMonitorLib'), without .dll
 
-from OpenHardwareMonitor.Hardware import Computer
-
-# Initialize Computer object
-c = Computer()
-c.CPUEnabled = True  # get the Info about CPU
-c.GPUEnabled = True  # get the Info about GPU
-c.Open()
 
 def monitor_cpu():
-    while True:
-        for a in range(0, len(c.Hardware[0].Sensors)):
-            if "/amdcpu/0/power/0" in str(c.Hardware[0].Sensors[a].Identifier):
-                value = c.Hardware[0].Sensors[a].get_Value()
-                if value is not None:
-                    formatted_value = "{:.2f}".format(value)
-                    print(f"Cpu: {formatted_value}")
-                c.Hardware[0].Update()
-                time.sleep(3)  # Sleep to avoid busy-waiting
+    import clr  # the pythonnet module
+    clr.AddReference("LibreHardwareMonitorLib")
+    from LibreHardwareMonitor import Hardware
 
-def monitor_gpu():
-    while True:
-        for a in range(0, len(c.Hardware[1].Sensors)):
-            if "/atigpu/0/power/0" in str(c.Hardware[1].Sensors[a].Identifier):
-                value = c.Hardware[1].Sensors[a].get_Value()
-                if value is not None:
-                    formatted_value = "{:.2f}".format(value)
-                    print(f"Gpu: {formatted_value}")
-                c.Hardware[1].Update()
-                time.sleep(3)  # Sleep to avoid busy-waiting
+    computer = Hardware.Computer()
+    computer.IsCpuEnabled = True
+    computer.IsGpuEnabled = True
+    computer.Open()
+   # while True:
+    for hardware in computer.Hardware:
+        if hardware.HardwareType == Hardware.HardwareType.Cpu or hardware.HardwareType == Hardware.HardwareType.GpuNvidia or hardware.HardwareType == Hardware.HardwareType.GpuAmd:
+            hardware.Update()
+            for sensor in hardware.Sensors:
+                if sensor.SensorType == Hardware.SensorType.Power:
+                    value = "{:.2f}".format(sensor.Value)
+                    print(f"{hardware.Name} - {sensor.Name}: {value} ")
+    computer.Close()
 
 def get_cpu_usage():
     """Returns the current CPU usage percentage."""
@@ -69,6 +56,7 @@ def get_disk_usage():
 
 
 def main():
+    monitor_cpu()
     print("CPU Usage: {}%".format(get_cpu_usage()))
 
     ram_usage = get_ram_usage()
@@ -77,7 +65,6 @@ def main():
     print("Available: {:.2f} GB".format(ram_usage['available'] / (1024 ** 3)))
     print("Used: {:.2f} GB".format(ram_usage['used'] / (1024 ** 3)))
     print("Percent: {}%".format(ram_usage['percent']))
-
     print("\nDisk Usage:")
     for partition in get_disk_usage():
         print("Device: {}".format(partition['device']))
--- teste/client.py
+++ teste/client.py
@@ -1,42 +1,19 @@
 import clr
-import socket
-import time
+clr.AddReference("LibreHardwareMonitorLib")
+from LibreHardwareMonitor import Hardware
 
-clr.AddReference(r'OpenHardwareMonitorLib')
-from OpenHardwareMonitor.Hardware import Computer
 
-# Initialize Computer object
-c = Computer()
-c.CPUEnabled = True
-c.GPUEnabled = True
-c.Open()
+computer = Hardware.Computer()
+computer.IsCpuEnabled = True
+computer.IsGpuEnabled = True
+computer.Open()
 
-# Define server parameters
-HOST = '127.0.0.1'  # Server IP address
-PORT = 65432  # Server port
-client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+for hardware in computer.Hardware:
+    if hardware.HardwareType == Hardware.HardwareType.Cpu or hardware.HardwareType == Hardware.HardwareType.GpuNvidia or hardware.HardwareType == Hardware.HardwareType.GpuAmd:
+        hardware.Update()
+        for sensor in hardware.Sensors:
+            if sensor.SensorType == Hardware.SensorType.Power:
+                value="{:.2f}".format(sensor.Value)
+                print(f"{hardware.Name} - {sensor.Name}: {value} ")
 
-while True:
-    for a in range(0, len(c.Hardware[0].Sensors)):
-        if "/amdcpu/0/power/0" in str(c.Hardware[0].Sensors[a].Identifier):
-            cpu_value = c.Hardware[0].Sensors[a].get_Value()
-            if cpu_value is not None:
-                cpu_value = "{:.2f}".format(cpu_value)
-            c.Hardware[0].Update()
-
-    for a in range(0, len(c.Hardware[1].Sensors)):
-        if "/atigpu/0/power/0" in str(c.Hardware[1].Sensors[a].Identifier):
-            gpu_value = c.Hardware[1].Sensors[a].get_Value()
-            if gpu_value is not None:
-                gpu_value = "{:.2f}".format(gpu_value)
-            c.Hardware[1].Update()
-
-    # Send data to the server
-    if cpu_value is not None:
-        data = f'{cpu_value},{gpu_value}'.encode()
-        client_socket.sendall(data)
-
-        print('Data sent to server:', data.decode())
-
-    time.sleep(1)  # Sleep to avoid busy-waiting
+computer.Close()
